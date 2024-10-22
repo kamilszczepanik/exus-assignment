55837cad3889deb70cdc6f47cf4a430cdc12293c
kamilszczepanik <kszczepanikworkspace@gmail.com>
2024-10-22 20:41:53 +0200

    [feature] Added basic setup for ws in backend

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,15 @@
 from fastapi import APIRouter
 
-from app.api.routes import forecast, history, items, login, stations, users, utils
+from app.api.routes import (
+    forecast,
+    history,
+    items,
+    login,
+    stations,
+    users,
+    utils,
+    websockets,
+)
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -10,3 +19,4 @@ api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(forecast.router, prefix="/forecast", tags=["forecast"])
 api_router.include_router(stations.router, prefix="/stations", tags=["stations"])
 api_router.include_router(history.router, prefix="/history", tags=["history"])
+api_router.include_router(websockets.router, prefix="/ws", tags=["ws"])
